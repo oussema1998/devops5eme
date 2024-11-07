f9a46466ba95c283c219839382c4f26c87165ead
sayda bouhoula <saidabouhoula84@gmail.com>
2024-11-07 19:01:58 +0100

    update code

--- tpFoyer-17/src/main/java/tn/esprit/tpfoyer17/controllers/BlocController.java
+++ tpFoyer-17/src/main/java/tn/esprit/tpfoyer17/controllers/BlocController.java
@@ -15,9 +15,12 @@ import java.util.List;
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @RequestMapping("api/blocs")
-
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 
 public class BlocController {
+
+    IBlocService blocService;
+
     @GetMapping("/findByFoyerIdFoyer/{idFoyer}")
     public List<Bloc> findByFoyerIdFoyer(@PathVariable("idFoyer") long idFoyer) {
         return blocService.findByFoyerIdFoyer(idFoyer);
@@ -28,31 +31,25 @@ public class BlocController {
         return blocService.findByChambresIdChambre(idChambre);
     }
 
-    IBlocService blocService;
-
     @GetMapping("/retrieveBlocs")
     public List<Bloc> retrieveBlocs() {
         return blocService.retrieveBlocs();
     }
-
 
     @GetMapping("/retrieveBloc/{idBloc}")
     public Bloc retrieveBloc(@PathVariable("idBloc") long idBloc) {
         return blocService.retrieveBloc(idBloc);
     }
 
-
     @PostMapping("/addBloc")
     public Bloc addBloc(@RequestBody Bloc bloc) {
         return blocService.addBloc(bloc);
     }
 
-
     @PutMapping("/updateBloc")
     public Bloc updateBloc(@RequestBody Bloc bloc) {
         return blocService.updateBloc(bloc);
     }
-
 
     @DeleteMapping("/removeBloc/{idBloc}")
     public void removeBloc(@PathVariable("idBloc") long idBloc) {
