3502e0aa5f41648bf7521dd10595a7ad3dae4a88
Ichrakblk <bouleklaka.ichrak@esprit.tn>
2024-11-08 14:41:35 +0100

    new

---
+++ src/test/java/tn/esprit/tpfoyer17/ExampleServiceTest.java
@@ -0,0 +1,13 @@
+package tn.esprit.tpfoyer17.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleServiceTest {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result, "L'addition devrait être correcte");
+    }
+}
